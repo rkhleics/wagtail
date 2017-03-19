b92d8b023931283fe62aeca3342bddd70f47edf9
Andy Babic <ababic@rkh.co.uk>
2017-03-19 17:05:56 +0000

    Fix __init__.py

--- wagtail/contrib/modeladmin/helpers/__init__.py
+++ wagtail/contrib/modeladmin/helpers/__init__.py
@@ -1,14 +1,14 @@
 from .url import AdminURLHelper, PageAdminURLHelper
 from .permission import PermissionHelper, PagePermissionHelper
-from .button import IntrospectiveButtonHelper
-from .deprecated import ButtonHelper as BH, PageButtonHelper as PBH
-
+from .button import GenericButtonHelper
+from .deprecated import ButtonHelper as Bh, PageButtonHelper as Pbh
 
 # TODO - Add deprecation warnings when importing the below
 
-class ButtonHelper(BH):
+
+class ButtonHelper(Bh):
     pass
 
 
-class PageButtonHelper(PBH):
+class PageButtonHelper(Pbh):
     pass
