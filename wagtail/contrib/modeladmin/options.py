7b25c9a8d1023cf87f1aaaf9bac0f1d64718d982
Andy Babic <ababic@rkh.co.uk>
2017-03-20 12:17:29 +0000

    Add docstrings and additional comments new methods on ModelAdmin class

--- wagtail/contrib/modeladmin/options.py
+++ wagtail/contrib/modeladmin/options.py
@@ -256,6 +256,13 @@ class ModelAdmin(WagtailRegisterable):
         return mark_safe(self.empty_value_display)
 
     def get_index_view_button_names(self, request):
+        """
+        Return a list or tuple of 'codenames' for buttons that should be
+        displayed in the InspectView for instances of `self.model`, in the
+        order that those buttons should be displayed. Values can also be a
+        tuple in the format (label, codename_list_for_dropdown) to create a
+        dropdown menu
+        """
         if self.index_view_button_names is not None:
             return self.index_view_button_names
         if self.is_pagemodel:
@@ -267,6 +274,12 @@ class ModelAdmin(WagtailRegisterable):
         return ('inspect', 'edit', 'delete')
 
     def get_inspect_view_button_names(self, request):
+        """
+        Return a list or tuple of 'codenames' for buttons that should be
+        displayed in the InspectView for instances of `self.model`, in the
+        order that those buttons should be displayed. Only flat lists are
+        supported here
+        """
         if self.inspect_view_button_names is not None:
             return self.inspect_view_button_names
         if self.is_pagemodel:
@@ -274,14 +287,41 @@ class ModelAdmin(WagtailRegisterable):
         return ('edit', 'delete')
 
     def get_button_url_for_action(self, codename, obj):
+        """
+        Return a URL to be used as the `href` attribut for buttons with action
+        `codename` for `obj` (an instance of `self.model` or `None`)
+        """
+        # If a value is defined as a class attribute, return that
+        action_specific_url_attr = '%s_button_url' % codename
+        if hasattr(self, action_specific_url_attr):
+            return getattr(self, action_specific_url_attr)
         return self.url_helper.get_action_url_for_obj(codename, obj)
 
     def get_button_label_for_action(self, codename, obj):
+        """
+        Return a string to be used as the `label` text for buttons with action
+        `codename` for `obj` (an instance of `self.model` or `None`)
+        """
+        # If a value is defined as a class attribute, return that
+        action_specific_label_attr = '%s_button_label' % codename
+        if hasattr(self, action_specific_label_attr):
+            return getattr(self, action_specific_label_attr)
+        # Action-specific overrides
         if codename == 'create':
             return _('Add %s') % self.model_verbose_name
+        # Default
         return codename.replace('_', ' ').capitalize()
 
     def get_button_title_for_action(self, codename, obj):
+        """
+        Return a string to be used as the `title` text for buttons with action
+        `codename` for `obj` (an instance of `self.model` or `None`)
+        """
+        # If a value is defined as a class attribute, return that
+        action_specific_title_attr = '%s_button_title' % codename
+        if hasattr(self, action_specific_title_attr):
+            return getattr(self, action_specific_title_attr)
+        # Action-specific overrides
         if codename == 'create':
             return _('Create a new %s') % self.model_verbose_name
         if codename == 'dropdown':
@@ -290,6 +330,11 @@ class ModelAdmin(WagtailRegisterable):
             return _("Preview draft version of '%s'") % obj
         if codename == 'view_live':
             return _("View live version of '%s'") % obj
+        if codename == 'revisions_index':
+            return _("View revision history for '%s'") % obj
+        if codename == 'add_subpage':
+            return _("Add child page to '%s'") % obj
+        # Default
         return _("%(action)s %(model_name)s '%(object_str)s'") % {
             'action': codename.replace('_', ' ').capitalize(),
             'model_name': self.model_verbose_name,
@@ -297,6 +342,10 @@ class ModelAdmin(WagtailRegisterable):
         }
 
     def get_button_css_classes_for_action(self, codename, obj):
+        """
+        Returns a list of css classes to be added to buttons with action
+        `codename` for `obj` (an instance of `self.model` or `None`)
+        """
         classes = list(self.default_button_css_classes)
         action_specific_list_attr = '%s_button_css_classes' % codename
         if hasattr(self, action_specific_list_attr):
