2e5bbcdc15c5e32fd018bcdaf99e1719d1d0f811
Andy Babic <ababic@rkh.co.uk>
2019-01-27 08:33:25 +0000

    Move fix_proxy_model_permissions() to wagtail.core.signal_handlers and move django version conditional to the new method

--- wagtail/core/signal_handlers.py
+++ wagtail/core/signal_handlers.py
@@ -1,11 +1,13 @@
 import logging
 
 import django
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from django.core.cache import cache
+from django.db.models import Q
 from django.db.models.signals import post_delete, post_save, pre_delete, post_migrate
 
 from wagtail.core.models import Page, Site
-from wagtail.core.contenttypes import fix_proxy_model_permissions
 
 
 logger = logging.getLogger('wagtail.core')
@@ -31,6 +33,37 @@ def post_delete_page_log_deletion(sender, instance, **kwargs):
     logger.info("Page deleted: \"%s\" id=%d", instance.title, instance.id)
 
 
+def fix_proxy_model_permissions(**kwargs):
+    if django.VERSION >= (2, 2):
+        return
+
+    for model in django.apps.get_models():
+        opts = model._meta
+
+        if not opts.proxy:
+            continue
+
+        proxy_content_type, created = ContentType.objects.get_or_create(
+            app_label=opts.app_label,
+            model=opts.model_name,
+        )
+
+        concrete_content_type = ContentType.objects.get_for_model(model, for_concrete_model=True)
+
+        default_permission_codenames = [
+            '%s_%s' % (action, opts.model_name)
+            for action in opts.default_permissions
+        ]
+        permissions_query = Q(codename__in=default_permission_codenames)
+        for codename, name in opts.permissions:
+            permissions_query |= Q(codename=codename, name=name)
+
+        Permission.objects.filter(
+            permissions_query,
+            content_type=concrete_content_type
+        ).update(content_type=proxy_content_type)
+
+
 def register_signal_handlers():
     post_save.connect(post_save_site_signal_handler, sender=Site)
     post_delete.connect(post_delete_site_signal_handler, sender=Site)
@@ -38,5 +71,4 @@ def register_signal_handlers():
     pre_delete.connect(pre_delete_page_unpublish, sender=Page)
     post_delete.connect(post_delete_page_log_deletion, sender=Page)
 
-    if django.VERSION < (2, 2):
-        post_migrate.connect(fix_proxy_model_permissions)
+    post_migrate.connect(fix_proxy_model_permissions)
